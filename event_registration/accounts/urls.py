56eb1416364719c1f534ce39cac0093871f67154
joshgervais <joshgervais1324@gmail.com>
2024-04-18 12:15:42 -0700

    fixed recursive error

--- event_registration/accounts/urls.py
+++ event_registration/accounts/urls.py
@@ -1,10 +1,7 @@
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
